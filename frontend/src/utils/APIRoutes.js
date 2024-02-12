b2696428ba25aad57941ceadae4c679efccb6394
Durgesh Mehar <durgeshmehar2002@gmail.com>
2024-02-12 18:58:31 +0530

    D1 : Previos deployment

--- frontend/src/utils/APIRoutes.js
+++ frontend/src/utils/APIRoutes.js
@@ -1,11 +1,3 @@
-// export const host = "http://localhost:5000"
-
-// export const registerRoute = `${host}/user/register`
-// export const loginRoute = `${host}/user/login`
-// export const setAvatarRoute = `${host}/user/setavatar`
-// export const getContactsRoute = `${host}/user/allcontacts`
-// export const messageAddRoute = `${host}/message/add`
-// export const messageGetRoute = `${host}/message/getAll`
 
 export const host= "http://durgeshchat.vercel.app/";
 // export const host="http://localhost:5000/"
