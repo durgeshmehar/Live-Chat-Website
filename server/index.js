f1309af174922bc74ae193873a32b3c30f37332d
Durgesh Mehar <durgeshmehar2002@gmail.com>
2024-02-12 13:37:15 +0530

    Deployment 1 : Add server url in io socket

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ const server = app.listen(process.env.PORT, () => {
 });
 const io = socket(server, {
   cors: {
-    origin: "*",
+    origin: "http://durgeshchat.vercel.app/",
     credentials: true,
   },
 });
